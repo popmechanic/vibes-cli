c257442e1b2da48e7d6ca44fb7ae19ea1cb0c47a
popmechanic <marcus.e@gmail.com>
2026-03-19 00:58:03 -0700

    chore: sync electrobun version to 0.1.93, update icons, add deploy-api lockfile

--- vibes-desktop/electrobun.config.ts
+++ vibes-desktop/electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "VibesOS",
 		identifier: "com.vibes.os",
-		version: "0.1.86",
+		version: "0.1.93",
 	},
 	build: {
 		mac: { bundleCEF: false, codesign: true, notarize: true },
