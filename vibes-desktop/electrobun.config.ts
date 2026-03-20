758401f090f89f47d431f02e25618f1633e59634
popmechanic <marcus.e@gmail.com>
2026-03-19 20:48:56 -0700

    bump version to 0.1.95

--- vibes-desktop/electrobun.config.ts
+++ vibes-desktop/electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "VibesOS",
 		identifier: "com.vibes.os",
-		version: "0.1.94",
+		version: "0.1.95",
 	},
 	build: {
 		mac: { bundleCEF: false, codesign: true, notarize: true },
